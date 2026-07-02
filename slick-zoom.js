e44e6bff0099296792c27945ce28ddf5982dd5a9
Samgu Lee <cable8mm@gmail.com>
2026-07-03 01:27:43 +0900

    refactor: all codes

--- slick-zoom.js
+++ slick-zoom.js
@@ -1,9 +1,16 @@
 $.fn.slickZoom = function (options) {
   options = options || {};
 
-  $(this).each(function(i) {
+  $(this).each(function (i) {
     var container = $(this);
-    options.item = options.item || '.' + container.children().attr('class');
+    var firstChild = container.children().first();
+
+    // Set default item selector if not provided
+    if (!options.item) {
+      var firstChildClass = firstChild.attr("class");
+      options.item = firstChildClass ? "." + firstChildClass : ".item";
+    }
+
     var items = container.find(options.item);
     var slickOptions = options.slickOptions || {
       dots: true,
@@ -12,68 +19,90 @@ $.fn.slickZoom = function (options) {
       slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: false,
-      accessibility: false
+      accessibility: false,
     };
 
-    var slickInstance = items.length >= 2 ? container.slick(slickOptions) : null;
+    var slickInstance =
+      items.length >= 2 ? container.slick(slickOptions) : null;
 
     // init photoswipe
     container
-      .attr('data-pswp-uid', i+1)
-      .on('click', options.item, function (e) {
+      .attr("data-pswp-uid", i + 1)
+      .on("click", options.item, function (e) {
         e.preventDefault();
         var slideItem = $(this);
-        var index = slideItem.index() - (slideItem.siblings('.slick-slide.slick-cloned').length / 2);
+        var index = getOriginalIndex(slideItem);
         openPhotoSwipe(index, container);
       });
 
+    function getOriginalIndex(slideItem) {
+      // Calculate the original index by accounting for cloned slides
+      var clonedSlides = slideItem.siblings(".slick-slide.slick-cloned");
+      var clonedCount = clonedSlides.length / 2;
+      return slideItem.index() - clonedCount;
+    }
+
     function openPhotoSwipe(index, $gallery) {
-      var pswpElement = $('.pswp')[0];
+      var pswpElement = $(".pswp")[0];
+
+      if (!pswpElement) {
+        console.error(
+          "PhotoSwipe element not found. Please add a .pswp element to your page.",
+        );
+        return;
+      }
+
       var images = parseImages($gallery);
 
-      var photoSwipeOptions = options.photoSwipeOptions || {
-        fullscreenEl: false,
-        index: parseInt(index, 10),
-        galleryUID: $gallery.attr('data-pswp-uid'),
-        history: false,
-        getThumbBoundsFn: function(index) {
-          // to make the open/close animation
-          var rect = images[index].el.getBoundingClientRect();
-          var pageYScroll = window.pageYOffset || document.documentElement.scrollTop;
+      if (!images || images.length === 0) {
+        console.error("No images found in gallery");
+        return;
+      }
 
-          return {
-            x: rect.left,
-            y: rect.top + pageYScroll,
-            w: rect.width
-          };
-        }
+      // Ensure index is within bounds
+      var safeIndex = Math.max(
+        0,
+        Math.min(parseInt(index, 10), images.length - 1),
+      );
+
+      var photoSwipeOptions = options.photoSwipeOptions || {
+        index: safeIndex,
+        galleryUID: $gallery.attr("data-pswp-uid"),
       };
 
       // Pass data to PhotoSwipe and initialize it
-      var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, images, photoSwipeOptions);
+      var gallery = new PhotoSwipe(pswpElement, images, photoSwipeOptions);
       gallery.init();
 
-      // slide the slick carouel in the background when the zoom carouel is moving
+      // slide the slick carousel in the background when the zoom carousel is moving
       if (slickInstance) {
-        gallery.listen('beforeChange', function() {
-          var slideItem = $(gallery.currItem.el).closest(options.item);
-          var currentIndex = slideItem.index() - (slideItem.siblings('.slick-slide.slick-cloned').length / 2);;
-          slickInstance.slick('slickGoTo', currentIndex);
+        gallery.on("change", function () {
+          if (!gallery.currSlide || !gallery.currSlide.data) {
+            return;
+          }
+
+          var slideItem = $(gallery.currSlide.data.el).closest(options.item);
+          if (slideItem.length) {
+            var currentIndex = getOriginalIndex(slideItem);
+            slickInstance.slick("slickGoTo", currentIndex);
+          }
         });
       }
-    };
-
-    function parseImages(el) {
-      return el.find(options.item + ':not(.slick-cloned) img').map(function() {
-        return {
-          el: this,
-          src: this.src,
-          msrc: this.src,
-          w: this.naturalWidth,
-          h: this.naturalHeight
-        };
-      }).get();
     }
 
+    function parseImages(el) {
+      return el
+        .find(options.item + ":not(.slick-cloned) img")
+        .map(function () {
+          return {
+            el: this,
+            src: this.src,
+            msrc: this.src,
+            w: this.naturalWidth || this.width || 800,
+            h: this.naturalHeight || this.height || 600,
+          };
+        })
+        .get();
+    }
   });
-}
+};
